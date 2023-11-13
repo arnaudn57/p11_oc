7cc4845fdcc01c653c31d86ccb60f411a868fc59
Arnaud Nicastro <arnaud.nicastro@gmail.com>
2023-11-13 18:23:05 +0100

    Host integration + Css

--- src/pages/Flat.jsx
+++ src/pages/Flat.jsx
@@ -22,39 +22,67 @@ export default function Flat() {
         }
     };
 
+    const reviews = (ratingCount) => {
+        let max_rating = 5;
+        let rating = parseInt(ratingCount);
+        console.log(rating)
+        //je veux afficher le nombre d'étoiles correspondant à la note et si moins de 5 alors remplir avec des étoiles vides
+        let stars = [];
+        for (let i = 1; i <= rating; i++) {
+            stars.push(<i className="fa-solid fa-star rating-icon-filled"></i>);
+        }
+        for (let i = 1; i <= max_rating - rating; i++) {
+            stars.push(<i className="fa-solid fa-star rating-icon-empty"></i>);
+        }
+        console.log(stars)
+        return stars;
+    }
+
     useEffect(() => {
         fetchData();
     }, [id]);
 
-    return (
-        <>
-            <div className="carousel-container">
-                {dataFetched ? (
-                    flat ? (
-                        <Carousel flatPictures={flat.pictures} />
-                    ) : (
-                        <p>Flat not found</p>
-                    )
+   return (
+    <>
+        <div className="carousel-container">
+            {dataFetched ? (
+                flat ? (
+                    <Carousel flatPictures={flat.pictures} />
                 ) : (
-                    <p>Loading...</p>
-                )}
-            </div>
-            {flat && (
-                <div className="flat-details">
-                    <h1 className="flat-title">{flat.title}</h1>
-                    <p className="flat-location">{flat.location}</p>
-                    <div className="tag-container">
-                        {flat.tags.map((tag, index) => (
-                            <Tag tagTitle={tag} key={tag.id || index} />
-                        ))}
-                    </div>
-                    <div className="collapse-container">
-                        <Collapse title="Description" items={[flat.description]}/>
-                        <Collapse title="Équipements" items={flat.equipments}/>
-                    </div>
-                    <p>{flat.price}</p>
-                </div>
+                    <p>Flat not found</p>
+                )
+            ) : (
+                <p>Loading...</p>
             )}
-        </>
-    );
+        </div>
+        {flat && (
+            <>
+                <div className="flat-details">
+                    <div className="flat-details-left">
+                        <h1 className="flat-title">{flat.title}</h1>
+                        <p className="flat-location">{flat.location}</p>
+                        <div className="tag-container">
+                            {flat.tags.map((tag, index) => (
+                                <Tag tagTitle={tag} key={tag.id || index} />
+                            ))}
+                        </div>
+                    </div>
+                    <div className="flat-details-right">
+                        <div className="host">
+                            <img className="host-picture" src={flat.host.picture} alt={flat.host.name} />
+                            <p className="host-name">{flat.host.name}</p>
+                        </div>
+                        <div className="rating">
+                            {flat.rating && reviews(flat.rating)}
+                        </div>
+                    </div>
+                </div>
+                <div className="collapse-container">
+                    <Collapse title="Description" items={[flat.description]} />
+                    <Collapse title="Équipements" items={flat.equipments} />
+                </div>
+            </>
+        )}
+    </>
+);
 }
